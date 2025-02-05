5f9bc7903cc8b8dcc6b3ab12551852d9186b09d9
Marceau <marceau.combet@gmail.com>
2025-02-05 18:59:47 +0100

    changed naming

--- scripts/preprocessing/polarize.py
+++ scripts/preprocessing/polarize.py
@@ -7,7 +7,7 @@ from pathlib import Path
 def polarizing(im_path:Path) -> Image:
     print("polarizing...")
     image = imageio.imread(im_path)
-    polarImage, _ = polarTransform.convertToPolarImage(image, initialRadius=0, hasColor=False,
+    polar_image, _ = polarTransform.convertToPolarImage(image, initialRadius=0, hasColor=False,
                                                             finalRadius=39, initialAngle=0,
                                                             finalAngle=2 * 3.141592)
-    return Image.fromarray(polarImage)
+    return Image.fromarray(polar_image)
